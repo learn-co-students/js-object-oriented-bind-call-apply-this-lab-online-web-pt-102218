f4cf65522bd0c98249680924ff7cb7c71b58c12a
bojosteph <bojosteph@gmail.com>
2019-05-25 17:06:22 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 //Your code here
+function justInvoke(fn) {
+  return fn();
+}
+
+function setThisWithCall(fn, name, age) {
+  return fn.call(name, age);
+};
+
+function setThisWithApply(fn, name, age ) {
+  return fn.apply(name, age);
+};
+
+function returnNewFunctionOf(fn, name) {
+  return fn.bind(name);
+}
