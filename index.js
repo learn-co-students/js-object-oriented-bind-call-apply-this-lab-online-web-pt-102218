46b663d5d4759825825d3f2ccbf02b8112c34a3b
Matthew Sherman <msherman0801@gmail.com>
2019-05-27 10:06:11 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 //Your code here
+function justInvoke(fn) {
+    return fn()
+}
+
+function setThisWithCall(returnsThisAndArgs, obj, age) {
+    return returnsThisAndArgs.call(obj, age)
+}
+
+function setThisWithApply(returnsThisAndArgs, obj, age) {
+    return returnsThisAndArgs.apply(obj, age)
+}
+
+function returnNewFunctionOf(functionToBeCopied, name) {
+    return functionToBeCopied.bind(name)
+}
